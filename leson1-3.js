a4f22cc11c48b612643e2cb9c40faa4468c4e388
Alecsandr Кozlovski <79801722+SENIA918@users.noreply.github.com>
2022-06-27 13:38:20 +0300

    Update leson1-3.js

--- leson1-3.js
+++ leson1-3.js
@@ -1,10 +1,9 @@
 //Задача3.Напишите функцию copyArr(arr), которая копирует массив не изменяя оригинал.
-   vegetables = ['Капуста', 'Репа', 'Редиска', 'Морковка'];
-function arrayClone(arr) {
-    let vegetables = ['Капуста', 'Репа', 'Редиска', 'Морковка'];
-    function copyArr(arr) {
-        return arr.slice(0);
-    };
-    let arr1 = copyArr(arr)(vegetables);
-    document.write(arr1);
+ const arrD = ['Капуста', 'Репа', 'Редиска', 'Морковка'];
+   function copyArr(arrD) {
+   let array = arrD;
+   return array.slice(0);
+
 }
+document.write(copyArr(arrD)); 
+
