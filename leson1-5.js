431feec0110ba5cbc44a62ca3870184a7acdcd4f
Alecsandr Кozlovski <79801722+SENIA918@users.noreply.github.com>
2022-06-27 13:40:07 +0300

    Update leson1-5.js

--- leson1-5.js
+++ leson1-5.js
@@ -4,8 +4,8 @@ const num = prompt('Введите число', 155639217);
 function colonOdd (num) {
     let str = num.toString();
     let result = [str[0]];
-    for(let i=1; i<str.length; i++) {
-        if((str[i-1]%2 !== 0)&&(str[i]%2 !== 0)) {
+    for(let i = 1; i < str.length; i++) {
+        if((str[i-1] % 2 !== 0) && (str[i] % 2 !== 0)) {
             result.push(':', str[i]);
         }
         else {
