e4376f09559bc9a2b0a3ccc769ad9f5a52bbdbca
Alecsandr Кozlovski <79801722+SENIA918@users.noreply.github.com>
2022-06-27 13:38:57 +0300

    Update leson1-4.js

--- leson1-4.js
+++ leson1-4.js
@@ -1,18 +1,19 @@
 //Задача4.Напишите функцию, которая преобразовывает и объединяет все элементы массива (numbers + strings) в одно строковое значение. Элементы массива будут разделены запятой. Получите результат двумя разными методами (с использованием цикла и без него)
 //Функция должна принимать массив а возвращать строку.
-let arr = [1,2,'МАССИВ'];
-function arrShow() {
-    for (let arr1 of arr) {
+const arrays = [1,2,'МАССИВ'];
+
+function arrShows(arrays) {
+    for (let i = 0; i < arrays.length; i++) {
     }
-    alert(arr)
-}
-arrShow();
-
+    let str1 = arrays.toString();
+        return(str1);  
+    }
+    document.write(arrShows(arrays));
 //Пример 2
+const b = [1,2,'МАССИВ'];
 
-let array = [1,2,'МАССИВ'];
-function arrShow() {
-    let arr1 = array;
-    alert(arr1);
+function arrShow(){
+let str1 = b.join();
+    return(str1); 
 }
-arrShow();
+    document.write(arrShow());
